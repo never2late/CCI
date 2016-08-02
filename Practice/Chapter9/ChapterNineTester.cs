620a0c14186dad1403492a6b55ed4b604cb66dd9
never2late <obeynheed@gmail.com>
2016-08-02 23:44:28 +0900

    Ch.9 finished

--- Practice/Chapter9/ChapterNineTester.cs
+++ Practice/Chapter9/ChapterNineTester.cs
@@ -109,7 +109,7 @@ namespace Practice
             else if (q == 2)
             {
                 string[] s = { "silent", "abcdef", "kfc", "listen", "tenlis", "a" };
-                
+
                 QuestionTwo(s);
                 var sb = new StringBuilder();
                 for (int i = 0; i < s.Length; i++)
@@ -118,6 +118,88 @@ namespace Practice
                 }
 
                 PrintLn(sb);
+            }
+            else if (q == 3)
+            {
+                int[] n = { 15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14 };
+                int num = 5;
+
+                var index = QuestionThree(n, num);
+
+                PrintLn("Array : " + n.toString());
+                PrintLn("Searching for " + num);
+                PrintLn((index >= 0) ? "FOUND at " + index : "NOT FOUND");
+            }
+            else if (q == 4)
+            {
+                QuestionFour();
+            }
+            else if (q == 5)
+            {
+                string[] arr = { "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", "" };
+                string find = "ball";
+                int index = QuestionFive(arr, find);
+
+                PrintLn("Array : " + arr.toString());
+                PrintLn("Searching for " + find);
+                PrintLn((index >= 0) ? "FOUND at " + index : "NOT FOUND");
+            }
+            else if (q == 6)
+            {
+                int w = 3, l = 8;
+                var n = new int[l, w];
+                int x = -1, y = -1;
+                int find = 20;
+
+                int cnt = 0;
+                for (int j = 0; j < l; j++)
+                {
+                    for (int i = 0; i < w; i++)
+                    {
+                        n[j, i] = cnt++;
+                    }
+                }
+
+                QuestionSix(n, out x, out y, find);
+
+                PrintLn(n.toString());
+                PrintLn((x == -1) ? find + " NOT FOUND " : find + " FOUND : (" + x + ", " + y + ")");
+            }
+            else if (q == 7)
+            {
+                //Person[] p = {
+                //    new Person(65, 100),
+                //    new Person(70, 150),
+                //    new Person(56, 90),
+                //    new Person(75, 190),
+                //    new Person(60, 95),
+                //    new Person(68, 110)
+                //};
+                Person[] p = {
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                    new Person(),
+                };
+
+                PrintLn(p.toString());
+
+                var result = QuestionSeven(p);
+                var listHeight = result.Item1;
+                var listWeight = result.Item2;
+                var bigger = result.Item3;
+
+                PrintLn("HeightList : " + listHeight.ToArray().toString());
+                PrintLn("WeightList : " + listWeight.ToArray().toString());
+                PrintLn("RESULT : " + bigger.ToArray().toString());
             }
         }
 
@@ -347,23 +429,23 @@ namespace Practice
 
         public void QuestionTwo(string[] s)
         {
-			Array.Sort(s, new AnagramComparer());
+            Array.Sort(s, new AnagramComparer());
         }
 
-		private class AnagramComparer : IComparer<String>
-		{
-			public int Compare(String s1, String s2)
-			{
-				var c1 = s1.ToArray();
-				var c2 = s2.ToArray();
-				Array.Sort(c1);
-				Array.Sort(c2);
-				var a1 = new string(c1);
-				var a2 = new string(c2);
+        private class AnagramComparer : IComparer<String>
+        {
+            public int Compare(String s1, String s2)
+            {
+                var c1 = s1.ToArray();
+                var c2 = s2.ToArray();
+                Array.Sort(c1);
+                Array.Sort(c2);
+                var a1 = new string(c1);
+                var a2 = new string(c2);
 
-				return a1.CompareTo(a2);
-			}
-		}
+                return a1.CompareTo(a2);
+            }
+        }
 
         public void quickSort(char[] n)
         {
@@ -401,6 +483,170 @@ namespace Practice
             char tmp = n[s];
             n[s] = n[e];
             n[e] = tmp;
+        }
+        //3, 4, 5, 1, 2
+        //4, 5, 1, 2, 3
+        public int QuestionThree(int[] n, int num)
+        {
+            int s = 0, e = n.Length - 1;
+            for (int m = (s + e) / 2; s <= e; m = (s + e) / 2)
+            {
+                if (n[m] == num) return m;
+                else if (n[s] < n[m])
+                {
+                    if (n[s] <= num && num < n[m]) e = m - 1;
+                    else s = m + 1;
+                }
+                else
+                {
+                    if (n[m] < num && num <= n[e]) s = m + 1;
+                    else e = m - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public void QuestionFour()
+        {
+
+        }
+
+        public int QuestionFive(string[] arr, string str)
+        {
+            return QuestionFive(arr, str, 0, arr.Length - 1);
+        }
+
+        public int QuestionFive(string[] arr, string str, int s, int e)
+        {
+            for (int m = (s + e) / 2; s <= e; m = (s + e) / 2)
+            {
+                if (arr[m].Equals(str) == true) return m;
+                if (arr[m] == "")
+                {
+                    var result = QuestionFive(arr, str, s, m - 1);
+                    if (result >= 0) return result;
+
+                    return QuestionFive(arr, str, m + 1, e);
+                }
+                else if (str.CompareTo(arr[m]) < 0) e = m - 1;
+                else s = m + 1;
+            }
+
+            return -1;
+        }
+
+        public void QuestionSix(int[,] n, out int x, out int y, int num)
+        {
+            x = -1; y = -1;
+            var l = n.GetLength(0);
+            var w = n.GetLength(1);
+            int sx = 0, sy = 0, ex = w - 1, ey = l - 1;
+
+            for (int midy = (sy + ey) >> 1; sy <= ey; midy = (sy + ey) >> 1)
+            {
+                if (n[midy, 0] <= num && num <= n[midy, ex])
+                {
+                    for (int midx = (sx + ex) >> 1; sx <= ex; midx = (sx + ex) >> 1)
+                    {
+                        if (num == n[midy, midx])
+                        {
+                            x = midx;
+                            y = midy;
+                            return;
+                        }
+                        else if (num < n[midy, midx]) ex = midx - 1;
+                        else sx = midx + 1;
+                    }
+
+                    return;
+                }
+                else if (num < n[midy, 0]) ey = midy - 1;
+                else sy = midy + 1;
+            }
+        }
+
+        public Tuple<List<Person>, List<Person>, List<Person>> QuestionSeven(Person[] p)
+        {
+            var byHeight = new Person[p.Length];
+            var byWeight = new Person[p.Length];
+            var listHeight = new List<Person>();
+            var listWeight = new List<Person>();
+            p.CopyTo(byHeight, 0);
+            p.CopyTo(byWeight, 0);
+            Array.Sort(byHeight, new HeightComparer());
+            Array.Sort(byWeight, new WeightComparer());
+
+            listHeight.Add(byHeight[0]);
+            var weight = byHeight[0].w;
+            for (int i = 1; i < byHeight.Length; i++)
+            {
+                var person = byHeight[i];
+                if (person.w > weight)
+                {
+                    weight = person.w;
+                    listHeight.Add(person);
+                }
+            }
+
+            listWeight.Add(byWeight[0]);
+            var height = byWeight[0].h;
+            for (int i = 1; i < byWeight.Length; i++)
+            {
+                var person = byWeight[i];
+                if (person.h > height)
+                {
+                    height = person.h;
+                    listWeight.Add(person);
+                }
+            }
+
+            var bigger = (listHeight.Count > listWeight.Count) ? listHeight : listWeight;
+            return new Tuple<List<Person>, List<Person>, List<Person>>(listHeight, listWeight, bigger);
+        }
+
+        public class Person
+        {
+            public int h { get; set; }
+            public int w { get; set; }
+            private static Random r = new Random(DateTime.Now.Millisecond);
+
+            public Person()
+            {
+                this.h = r.Next(1, 100);
+                this.w = r.Next(1, 100);
+            }
+
+            public Person(int h, int w)
+            {
+                this.h = h;
+                this.w = w;
+            }
+
+            public override string ToString()
+            {
+                return "(" + h + "," + w + ")";
+            }
+        }
+
+        private class HeightComparer : IComparer<Person>
+        {
+            public int Compare(Person p1, Person p2)
+            {
+                if (p1.h == p2.h) return 0;
+                else if (p1.h < p2.h) return -1;
+                return 1;
+            }
+        }
+
+        private class WeightComparer : IComparer<Person>
+        {
+            public int Compare(Person p1, Person p2)
+            {
+                if (p1.w == p2.w) return 0;
+                else if (p1.w < p2.w) return -1;
+                return 1;
+            }
         }
     }
 }
--- Practice/Common/UtilExt.cs
+++ Practice/Common/UtilExt.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Practice.ChapterNineTester;
 
 namespace Practice.Common
 {
@@ -22,6 +23,56 @@ namespace Practice.Common
 
             sb.Remove(sb.Length - 2, 2);
             sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        public static string toString(this string[] arr)
+        {
+            if (arr == null) return "";
+
+            var sb = new StringBuilder();
+            sb.Append("{");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sb.Append(arr[i] + ", ");
+            }
+
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        public static string toString(this int[,] n)
+        {
+            int l = n.GetLength(0);
+            int w = n.GetLength(1);
+            var sb = new StringBuilder();
+
+            for (int j = 0; j < l; j++)
+            {
+                for (int i = 0; i < w; i++)
+                {
+                    sb.Append(n[j, i] + ", ");
+                }
+                sb.Remove(sb.Length - 2, 2);
+                sb.Append("\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public static string toString(this Person[] p)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < p.Length; i++)
+            {
+                sb.Append(p[i].ToString() + ", ");
+            }
+
+            sb.Remove(sb.Length - 2, 2);
 
             return sb.ToString();
         }
