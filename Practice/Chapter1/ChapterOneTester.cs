27e4e4a369ab7454a916db35959fa44f7ed5afcd
never2late <obeynheed@gmail.com>
2016-08-10 00:00:18 +0900

    ch.1 q. 6

--- Practice/Chapter1/ChapterOneTester.cs
+++ Practice/Chapter1/ChapterOneTester.cs
@@ -45,6 +45,61 @@ namespace Practice
                     PrintLn("str: " + str + " - removeDup: " + result);
                 }
             }
+            else if (q == 4)
+            {
+                string[] arr =
+                {
+                    "ABCD", "BCDA", "ABC", "BCD", "AA", "AAAA", "", "A", "LISTEN", "SILENT"
+                };
+
+                for (var i = 0; i < arr.Length - 1; i += 2)
+                {
+                    var s1 = arr[i];
+                    var s2 = arr[i + 1];
+                    var result = QuestionFour(s1, s2);
+                    PrintLn(s1 + ", " + s2 + " - isAnagram : " + result);
+                }
+            }
+            else if (q == 5)
+            {
+                string[] arr =
+                {
+                    "ABCD", "A B C", "A B C ", " ", "   ", "", "A       B "
+                };
+
+                foreach (var str in arr)
+                {
+                    var result = QuestionFive(str);
+                    PrintLn("str: " + str + " - mix: " + result);
+                }
+            }
+            else if (q == 6)
+            {
+                int n = 5;
+                var arr1 = new char[5, 5];
+                char c = 'a';
+                for (int i = 0; i < arr1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr1.GetLength(1); j++)
+                    {
+                        arr1[i, j] = c++;
+                        Print(arr1[i,j] + "");
+                    }
+                    PrintLn("");
+                }
+
+                PrintLn("===== AFTER =====");
+                var result = QuestionSix(arr1);
+
+                for (int i = 0; i < result.GetLength(0); i++)
+                {
+                    for (int j = 0; j < result.GetLength(1); j++)
+                    {
+                        Print(arr1[i, j] + "");
+                    }
+                    PrintLn("");
+                }
+            }
 		}
 
 		private bool QuestionOne(string str, int option = 0)
@@ -168,6 +223,73 @@ namespace Practice
             }
 
             return "";
+        }
+
+        private bool QuestionFour(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2) || s1.Length != s2.Length) return false;
+
+            var a1 = s1.ToArray();
+            var a2 = s2.ToArray();
+            Array.Sort(a1);
+            Array.Sort(a2);
+
+            return new string(a1).Equals(new string(a2));
+        }
+
+        private string QuestionFive(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return "";
+
+            int cnt = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                cnt = (str.ElementAt(i) == ' ') ? cnt + 1 : cnt;
+            }
+            //ab c--> ab%20c (4 --> 6)
+            //a b c --> a%20b%20c%20 (6 --> 12)
+            var arr = new char[str.Length + 2 * cnt];
+
+            int index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str.ElementAt(i);
+                if (c == ' ')
+                {
+                    arr[index++] = '%';
+                    arr[index++] = '2';
+                    arr[index++] = '0';
+                }
+                else
+                {
+                    arr[index++] = c;
+                }
+            }
+
+            return new string(arr);
+        }
+        //abcd      miea
+        //efgh      njfb
+        //ijkl      okgc
+        //mnop      plhd
+        private char[,] QuestionSix(char[,] arr)
+        {
+            int n = arr.GetLength(0);
+            var result = new char[n, n];
+
+            for (int i = 0; i < n >> 1; i++)
+            {
+                for (int j = i; j < n - i - 1; j++)
+                {
+                    var tmp = arr[i, j];
+                    arr[i, j] = arr[n - j - 1, i];
+                    arr[n - j - 1, i] = arr[n - i - 1, n - j - 1];
+                    arr[n - i - 1, n - j - 1] = arr[j, n - i - 1];
+                    arr[j, n - i - 1] = tmp;
+                }
+            }
+
+            return result;
         }
 	}
 }
